60506c1213c1b0260f2d1323bc994c21ef7ed92d
DaviFeitosa <90164085+DaviFeitosa21@users.noreply.github.com>
2025-09-01 23:54:01 -0300

    adicionando quick_sort

---
+++ atividade_1/quick_sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int particionar(int *vetor, int inicio, int fim) {
+    int randomPivo = inicio + rand() % (fim - inicio + 1); 
+
+    int pivo = vetor[randomPivo];
+    int i = inicio - 1;
+    int j = fim + 1;
+
+    while (1)
+    {
+        for(i = i + 1; vetor[i] < pivo; i++);
+
+        for(j = j - 1; vetor[j] > pivo; j--);
+
+        if(i >= j) {
+            return j;
+        }
+
+        int aux = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = aux;
+    }
+}
+
+void quickSort(int *vetor, int inicio, int fim) {
+    if(inicio < fim) {
+        int indicePivo = particionar(vetor, inicio, fim);
+        quickSort(vetor, inicio, indicePivo - 1);
+        quickSort(vetor, indicePivo + 1, fim);
+    }
+}
+
+int main() {
+
+    int vetor;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &vetor);
+
+    int *vetorMalloc = (int *)malloc(vetor * sizeof(int));
+
+    for(int i = 0; i < vetor; i++) {
+        printf("Digite um numero para a posicao %d: ", i + 1);
+        scanf("%d", &vetorMalloc[i]);
+    }
+
+    for(int i = 0; i < vetor; i++) {
+        printf("[%d] ", vetorMalloc[i]);
+    }
+
+    printf("\n");
+    quickSort(vetorMalloc, 0, vetor - 1);
+
+    for(int i = 0; i < vetor; i++) {
+        printf("[%d] ", vetorMalloc[i]);
+    }
+
+    free(vetorMalloc);
+
+    return 0;
+}
